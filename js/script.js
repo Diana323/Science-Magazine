a968437910d58c3f0c2036c3a28aafb02e7e468d
Diana323 <110489496+Diana323@users.noreply.github.com>
2022-10-29 22:30:23 +0300

    ........

--- js/script.js
+++ js/script.js
@@ -13,17 +13,29 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open2]"),
-//     closeModalBtn: document.querySelector("[data-modal-close2]"),
-//     modal: document.querySelector("[data-modal2]"),
-//   };
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
+// Get the modal
+var modal = document.getElementById('myModal');
 
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
